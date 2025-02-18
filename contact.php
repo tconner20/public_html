1e696d63400cdcb7748b26c6c2fde45ecd354288
tconner20 <46390503+tconner20@users.noreply.github.com>
2025-02-17 19:00:33 -0500

    added meta pixel update

--- contact.php
+++ contact.php
@@ -38,6 +38,26 @@
     <meta name="keywords" content="contact custom graphics Denver NC, vehicle wraps company Charlotte NC, request a quote, get in touch, best custom graphics company near me, contact us for vehicle wraps">
     <link rel="shortcut icon" href="assets/icons/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/styles.css">
+    
+    <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '3141447399452566');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=3141447399452566&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
+
 </head>
 
 <body>
